d4bd1ab5ee0c07baf6d86f5def296eff3c8ee8b1
Hala Bakhtiar <halabakhtiar@gmail.com>
2026-04-23 15:39:43 +0100

    database commenting

--- model/database.py
+++ model/database.py
@@ -1,8 +1,10 @@
 import sqlite3
 import os
 
+# specifying the name of the volog database
 DB_PATH = os.path.join(os.path.dirname(__file__), '..', 'volog.db')
 
+# getting a connection and 
 def get_connection():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
@@ -24,6 +26,7 @@ def init_db():
 
 def insert_entry(number_plate, headcount, gate_id):
     conn = get_connection()
+    # creates cursor internally and disregards it straight away after use
     conn.execute(
         'INSERT INTO entry_log (number_plate, headcount, gate_id) VALUES (?, ?, ?)',
         (number_plate, headcount, gate_id)
