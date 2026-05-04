ffebbcde7e1d4240c3841e27b3cddcd2634a4365
Hala Bakhtiar <halabakhtiar@gmail.com>
2026-05-04 09:36:37 +0100

    commenting app.py

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-# 
 from flask import Flask
 from flask_cors import CORS
 from mvc.model.database import init_db
 from mvc.controller.entry_controller import entry_bp
 
 app = Flask(__name__)
+
+# allow requests from other origins
 CORS(app)
+
+# register routes from entry crontroller
 app.register_blueprint(entry_bp)
 
 if __name__ == '__main__':
+
+    # initialise database before starting the app
     init_db()
+
+    # run in debug mode for development
     app.run(debug=True)
